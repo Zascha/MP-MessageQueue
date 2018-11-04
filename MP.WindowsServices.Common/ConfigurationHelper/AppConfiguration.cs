45601ccdd76038dadd1019e1da424d1896110ad1
AliaksandraZakharava <Aliaksandra_Zakharava@epam.com>
2018-11-04 19:44:26 +0300

    Finishing Client part. Version 11/4

---
+++ MP.WindowsServices.Common/ConfigurationHelper/AppConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+
+namespace MP.WindowsServices.Common.ConfigurationHelper
+{
+    internal class AppConfiguration : ConfigurationSection
+    {
+        [ConfigurationProperty("fileFilters")]
+        public FileFiltersElement FileFilters
+        {
+            get { return (FileFiltersElement)this["fileFilters"]; }
+        }
+        
+
+        [ConfigurationProperty("fileNameTemplate")]
+        public FileNameTemplateElement FileNameTemplate
+        {
+            get { return (FileNameTemplateElement)this["fileNameTemplate"]; }
+        }
+
+        [ConfigurationCollection(typeof(ObservableFolderElement), AddItemName = "folder")]
+        [ConfigurationProperty("observableFolders")]
+        public ConfigElementCollection<ObservableFolderElement> ObservableFolders
+        {
+            get { return (ConfigElementCollection<ObservableFolderElement>)this["observableFolders"]; }
+        }
+    }
+}
